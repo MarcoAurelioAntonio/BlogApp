db6fb3dd62e0afc6a4e1412af32574bf5506cb10
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2023-01-28 11:49:34 -0300

    validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,16 @@
 class Post < ApplicationRecord
+  # Associations
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   after_save :update_posts_counter
 
+  # Validations
   validates :title, presence: true, length: { maximum: 250 }, format: { with: /\A[a-zA-Z0-9 ]+\z/ }
+
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
+
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0, allow_nil: true }
 
   def update_posts_counter
