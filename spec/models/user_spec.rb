84440d139c6643361f9b20645e9f8d8619015a40
Marcus Aurelius <99001924+MarcoAurelioAntonio@users.noreply.github.com>
2023-01-28 12:35:54 -0300

    User test done

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+    subject { User.new(name: 'John', photo: 'rails generate rspec:install', bio: 'Master of the universe', posts_counter: 4) }
+
+    before { subject.save }
+
+    it 'is valid with valid attributes' do
+        expect(subject).to be_valid
+    end
+
+    it 'No name should be invalid' do
+        subject.name = nil
+        expect(subject).to_not be_valid
+    end
+
+    it 'Name length should be less than 50' do
+        subject.name = 'a' * 51
+        expect(subject).to_not be_valid
+    end
+
+    it 'Posts counter should be greater than or equal to 0' do
+        subject.posts_counter = -1
+        expect(subject).to_not be_valid
+    end
+
+    it 'Method recents_post should return the last 3 posts' do
+        expect(subject.recents_post).to eq(Post.limit(3).order(created_at: :desc))
+    end
+end
