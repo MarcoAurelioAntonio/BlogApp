7575f0dc8eeac7a9be238d4cdb6b5aa9940db72c
Gustavo Porcilio <gustavoporcilio@gmail.com>
2023-02-16 13:17:39 -0300

    commnets form view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root to: "users#index"
 
   resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+    end
   end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
